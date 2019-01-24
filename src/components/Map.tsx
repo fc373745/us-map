7437b975ce86d40a1fed65bc1cce702d0adc8f8e
francis <fc373745@gmail.com>
2019-01-24 05:56:53 -0800

    finished basic map chart

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,28 @@
 import { geoPath } from "d3-geo";
 import { select } from "d3-selection";
 import React, { useEffect } from "react";
-import { feature } from "topojson";
+import { createGlobalStyle } from "styled-components";
+import { feature, mesh } from "topojson";
 import us from "./us2.json";
+
+const GlobalStyle = createGlobalStyle`
+    .states {
+        fill: #373745;
+        &:hover {
+            fill: black;
+            transition-duration: 450ms;
+        }
+    }
+
+    .state-borders {
+        fill: none;
+        stroke: white;
+        stroke-width: 1px;
+        stroke-linejoin: round;
+        stroke-linecap: round;
+        pointer-events: none;
+    }
+`;
 
 const Map: React.FunctionComponent = () => {
     const mapRef = React.createRef<SVGSVGElement>();
@@ -20,9 +40,29 @@ const Map: React.FunctionComponent = () => {
             .data(feature(us, us.objects.states).features)
             .enter()
             .append("path")
+            .attr("class", "states")
             .attr("d", path as any);
+        map.append("path")
+            .attr(
+                "d",
+                //@ts-ignore
+                path(
+                    //@ts-ignore
+                    mesh(us, us.objects.states, function(a, b) {
+                        return a !== b;
+                    })
+                )
+            )
+            .attr("class", "state-borders");
     };
 
-    return <svg ref={mapRef} width={1000} height={800} />;
+    return (
+        <React.Fragment>
+            <svg ref={mapRef} width={1000} height={800}>
+                <GlobalStyle />
+            </svg>
+            ;
+        </React.Fragment>
+    );
 };
 export default Map;
